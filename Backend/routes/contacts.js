dc3f563fc834cb78ae3b01cd0da1b3f1c5f340cf
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-26 20:18:13 +0300

    Creating a contact API

--- Backend/routes/contacts.js
+++ Backend/routes/contacts.js
@@ -12,4 +12,26 @@ router.get('/', async(req,res) => {
     }
 });
 
+
+//Creating one 
+router.post('/', async(req,res) => {
+    const contact = new Contact({
+        full_name: req.body.full_name,
+        phone_number: req.body.phone_number,
+        relationship_status: req.body.relationship_status,
+        email: req.body.email,
+        longitude: req.body.longitude,
+        latitude: req.body.latitude
+    });
+
+
+    try{
+        const newContact = await contact.save();
+        res.status(201).json(newContact);
+    }catch(error){
+        //error 400 indicates if the user entered wrong data
+        res.status(400).json({ message: error.message });
+    }
+
+})
 module.exports = router
