5e1a2bd8dd40ee5a0626989d216e58715657d0be
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-26 16:39:40 +0300

    Folder structure

---
+++ Backend/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const app = express();
+
+//Import Routes
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+//connect to DB
+// mongoose.connect(
+//   process.env.DB_CONNECT,
+//   { useNewURLParser: true },
+//   () => console.log("connected to db")
+// );
+const username = "Batoul";
+const password = "B%401234";
+const cluster = "cluster0.bfprh5q";
+const dbname = "Address_Book";
+mongoose.connect(
+    `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
+const db = mongoose.connection;
+console.log(db)
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+//Middleware 
+//body parser to be able to use post requests 
+
+app.use(express.json());
+
+//Routes Middlewares
+
+app.use("/api/user", authRoute);
+
+app.listen(3000, () => console.log("Server up and running"));
