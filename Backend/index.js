5354f8d666dd7ed783545cbad57846194c880476
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-26 19:01:54 +0300

    Calling post route in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 //Import Routes
 const authRoute = require('./routes/auth');
-
+const postRoute = require('./routes/posts');
 dotenv.config();
 
 //connect to DB
@@ -40,5 +40,6 @@ app.use(express.json());
 //Routes Middlewares
 
 app.use("/api/user", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(3000, () => console.log("Server up and running"));
