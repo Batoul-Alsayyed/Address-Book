bf49a29b141ab3655329c35ae4041a6e73e47661
Batoul-Alsayyed <batoul.alsayyed20@gmail.com>
2022-06-30 02:14:56 +0300

    Adding routes inside App.js

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 import { Header } from './components/common'
+
+import Login from "./components/common/login/Login";
+import Register from "./components/common/register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/common/home/Home';
 
 function App() {
   return (
     <div className="App">
-       <Header />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </Router>
+        
     </div>
-
   );
 }
 
